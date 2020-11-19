4b42ec7732c72e6a1f6534bf66f5b2b4135625ba
Mrutyunjay Dash <mdash555@gmail.com>
2020-11-19 14:01:46 +0530

    templatefield modified

--- src/components/TemplateField.js
+++ src/components/TemplateField.js
@@ -8,19 +8,24 @@ class TemplateField extends Component {
     render() {
         return (
             <div className="container">
-                <div className="row center">
-                    <div className="input-field col s4">
-                        <label>Header Name</label>
-                        <input placeholder="" id="header_name" type="text" className="validate" />
-                    </div>
-                    <div className="input-field col s4">
-                        <label>DataType</label>
-                        <input placeholder="" id="data_type" type="text" className="validate" />
-                    </div>
-                    <div className="input-field col s4">
-                        <label for="disabled">DateForm</label>
-                        <input placeholder="" id="disabled" type="text" className="validate" />
-                    </div>
+                <div className="row center card-panel">
+                        <div className="input-field col s4">
+                            <label>Header Name</label>
+                            <input placeholder="" id="header_name" type="text" className="validate" />
+                        </div>
+                        <div className="input-field col s4">
+                            <label>DataType</label>
+                            <input placeholder="" id="data_type" type="text" className="validate" />
+                        </div>
+                        <div className="input-field col s4">
+                            <label for="date_form">DateForm</label>
+                            <input placeholder="" id="date_form" type="text" className="validate" />
+                        </div>
+                        <div className="field-icons right">
+                            <a className="waves-effect waves-light btn-small black" style={{marginRight:'10px'}} ><i className="small material-icons">add_box</i></a>
+                            <a className="waves-effect waves-light btn-small black"><i className="small material-icons">storage</i></a>
+                        </div>
+                        
                 </div>
             </div>
         )
