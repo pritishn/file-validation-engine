bce07d7dc673410c298284dae29cf0b193594f1c
Anurag Dash <anuragjeypore1234@gmail.com>
2020-11-21 23:36:16 +0530

    added showtemplatefield

--- src/components/ShowTemplate.js
+++ src/components/ShowTemplate.js
@@ -1,41 +1,76 @@
 import React, { Component } from "react";
 import database from "../datastore";
-
+import ShowTemplateField from "./ShowTemplateField"
 export default class ShowTemplate extends Component {
   state = {
-    name:"",
-    description:"",
-    fields:[],
-    groupRelations:"",
+    name: "ANURAGS TEMPLATE",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+    fields: [
+      {
+        headerName: "Field1",
+        dataType: "String",
+        isDateShowing: true,
+        dateType: "DD/MM/YYYY",
+        group: "1",
+        required: true,
+        regex: "s/s*",
+        collection: "employee",
+        databaseQuery: "randomdbQuery",
+      },
+      {
+        headerName: "",
+        dataType: "String",
+        isDateShowing: false,
+        dateType: "",
+        group: "",
+        required: false,
+        regex: "",
+        collection: "",
+        databaseQuery: "",
+      },
+    ],
+    groupRelations: "",
   };
 
-  componentDidMount(){
-    let template = database.templates.find(template => template.templateID == this.props.templateID);
-    this.setState({
-      name:template.name,
-      description:template.description,
-      fields:template.fields,
-      groupRelations:template.groupRelations
-    });
-  };
-  
+  componentDidMount() {
+    // let template = database.templates.find(
+    //   (template) => template.templateID == this.props.templateID
+    // );
+    // this.setState({
+    //   name: template.name,
+    //   description: template.description,
+    //   fields: template.fields,
+    //   groupRelations: template.groupRelations,
+    // });
+  }
+
   render() {
-    const fields = this.state.fields.map((field)=>{
+    const fields = this.state.fields.map((field) => {
       return (
         <div className="col s12 m12">
-          <hr></hr>
-
+          <ShowTemplateField fieldValues={field} />
         </div>
-      )
+      );
     });
     return (
-        <div className="container">
-          <div className="card">
-            <div className="row">
-
+      <div className="container">
+        <div className="row">
+        <form  className="col row s12 m12" onSubmit={this.createFields}>
+            <div className="col center s12 m12">
+              <label htmlFor="template-name">Template Name:</label>
+              <input id="template-name" name="template-name" value={this.state.name} required />
             </div>
-          </div>
+            <div className="col center s12 m12">
+              <label htmlFor="template-desc">Template Description:</label>
+              <input  className="materialize-textarea" id="template-desc" name="template-desc" value={this.state.description} required />
+            </div>
+            <br/><br/>
+            <div className="card">
+              {fields}
+            </div>
+          </form>
         </div>
+      </div>
     );
   }
 }
--- src/components/ShowTemplateField.js
+++ src/components/ShowTemplateField.js
@@ -1,55 +1,154 @@
 import React, { Component } from "react";
 import "@material-ui/core";
 import database from "../datastore";
-import { Card, TextField } from "@material-ui/core";
+
 
 class ShowTemplateField extends Component {
   state = {
-    headerName: "hn",
-    dataType: "dt",
-    dateType: "datet",
-    regex: "reg",
-    required: "req",
-    group: "g",
-    collection: "col",
-    databaseQuery: "db",
+    headerName: "",
+    dataType: "String",
+    isDateShowing: false,
+    dateType: "MM/DD/YYYY",
+    group: "",
+    required: false,
+    regex: "",
+    collection: "",
+    databaseQuery: "",
   };
-  //   state = {
-  //     headerName: "",
-  //     dataType: "",
-  //     dateType: "",
-  //     regex: "",
-  //     required: "",
-  //     group: "",
-  //     collection: "",
-  //     databaseQuery: "",
-  //   };
-
-  componentDidMount() {
-    //   let data = this.props.fieldData;
-    //   this.setState({
-    //       headerName:data.headerName,
-    //       dataType:data.dataType,
-    //       dateType:data.dateType,
-    //       regex: data.regex,
-    //       required:data.required,
-    //       group: data.group,
-    //       collection: data.collection,
-    //       databaseQuery: data.databaseQuery
-    //   });
+  // ... Data handler complete ...
+  async componentDidMount() {
+    let data = this.props.fieldValues;
+    this.setState({
+      headerName: data.headerName,
+      dataType: data.dataType,
+      dateType: data.dateType,
+      isDateShowing: data.isDateShowing,
+      regex: data.regex,
+      required: data.required,
+      group: data.group,
+      collection: data.collection,
+      databaseQuery: data.databaseQuery,
+    });
   }
+
   render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <input className="col s3 m3" value={this.state.headerName} readonly />
-          <input className="col s3 m3" value={this.state.dataType} readonly />
+    // ... Conditional Display ...
+    const displayDateSelector = this.state.isDateShowing ? (
+      <div className="input-field col s4 m3">
+        <input
+          className="validate"
+          name="dateFormat"
+          id="dateFormat"
+          defaultValue={this.state.dateType}
+        />
+        <label htmlFor="dateFormat">Date</label>
+      </div>
+    ) : (
+      <div className="input-field col s4 m3">
+        <input
+          className="validate"
+          name="dateFormat"
+          id="dateFormat"
+          defaultValue={this.state.dateType}
+          disabled
+        />
+         <label htmlFor="dateFormat">Date</label>
+      </div>
+    );
+
+    const displayRegex = (
+      <div className="row ">
+        <div className="col input-field s12 m12">
           <input
-            value={this.state.dateType}
-            className="col s3 m3"
-            style={{ display: this.state.dataType==="Date" ? "block" : "none" }}
-            readonly
+            name="regex"
+            id="regex"
+            className="validate"
+            placeholder="Regex"
+            defaultValue={this.state.regex}
           />
+          <label htmlFor="regex">Regex Query</label>
+        </div>
+      </div>
+    );
+
+    const displayDB = (
+      <div className="row input-field s12 m12">
+        <div className="col s6 m6">
+          <input
+            name="dbName"
+            id="dbName"
+            className="validate"
+            placeholder="Database Name"
+            defaultValue={this.state.collection}
+          />
+          <label htmlFor="dbName">Database Query</label>
+        </div>
+        <div className="col s6 m6">
+          <input
+            name="dbQuery"
+            id="dbQuery"
+            className="validate"
+            placeholder="Database Query"
+            defaultValue={this.state.databaseQuery}
+            required
+          />
+          <label htmlFor="dbQuery">Database Query</label>
+        </div>
+      </div>
+    );
+    // ... Conditional Display Complete...
+
+    return (
+      <div className="col s12 m12">
+        <div className="card-panel z-depth-2">
+          <div className="row">
+            <div className="input-field col s2 m2">
+              <input
+                placeholder="Header Name"
+                id="header_name"
+                type="text"
+                className="validate"
+                defaultValue={this.state.headerName}
+              />
+              <label htmlFor="header_name">Name</label>
+            </div>
+            <div className="input-field col s2 m2">
+              <input
+                defaultValue={this.state.dataType}
+                placeholder="Data Type"
+                className="validate"
+                name ="dataType"
+                id ="dataType"
+              />
+              <label htmlFor="dataType">Data</label>
+            </div>
+            {displayDateSelector}
+            <div className="input-field col s1 m1">
+              <input
+                placeholder="Group"
+                id="group_name"
+                name="group_name"
+                type="text"
+                className="validate"
+                defaultValue={this.state.group}
+              />
+              <label htmlFor="group_name">Group</label>
+            </div>
+            <div className="input-field col s1 m1">
+              <label>
+                <input
+                  defaultValue={this.state.required}
+                  className="filled-in"
+                  name="required"
+                  type="checkbox"
+                  defaultChecked={this.state.required}
+                />
+                <span>Required</span>
+              </label>
+            </div>
+            {displayRegex}
+            {displayDB}
+          </div>
         </div>
       </div>
     );
