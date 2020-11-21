d3a0c244eb57fa94553d0c97f14e31dccbd67019
Anurag Dash <anuragjeypore1234@gmail.com>
2020-11-21 20:55:58 +0530

    error card

--- src/components/ErrorLog.js
+++ src/components/ErrorLog.js
@@ -3,13 +3,26 @@ import React, { Component } from "react";
 import { saveAs } from "file-saver";
 
 export default class ErrorLog extends Component {
-  state = { errCode: 404, errMessage: "This is a dummy error file" };
+  state = {
+    errCode: 404,
+    errMessage: "This is a dummy error file",
+    fileName: "dummyFileName",
+  };
 
   handleClick = () => {
-    var blob = new Blob([this.state.errMessage], {
+    const fileName = this.state.fileName;
+    const errorMessage =
+      "===============ERROR_MESSAGE===============\n" +
+      "ErrorCode: " +
+      this.state.errCode +
+      "\n" +
+      "ErrorMessage: " +
+      this.state.errMessage;
+
+    var blob = new Blob([errorMessage], {
       type: "text/plain;charset=utf-8",
     });
-    saveAs(blob, "errorFile.txt");
+    saveAs(blob, fileName);
   };
 
   render() {
@@ -18,15 +31,17 @@ export default class ErrorLog extends Component {
         <div className="col s12 m6">
           <div className="card dell-blue z-depth-2 ">
             <div className="card-content white-text">
-              <span className="card-title">
+              <span className="card-title white-text">
                 Error Code: {this.state.errCode}
               </span>
-              <p>{this.state.errMessage}</p>
+              <div className="card card-content hoverable z-depth-2 ">
+                <span className="dell-blue-text">{this.state.errMessage}</span>
+              </div>
             </div>
-            <div className="card-action">
+            <div className="card-action ">
               <button
                 onClick={this.handleClick}
-                className="btn white dell-blue-text text-bold"
+                className="btn white dell-blue-text"
               >
                 Download Log
               </button>
