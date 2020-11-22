66c1d8b30d5a0cf9561ae053ceccae3be20193c7
Mrutyunjay Dash <mdash555@gmail.com>
2020-11-22 17:58:46 +0530

    footer fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-	  <footer className="">
-        {/* <div class="blue" style={{position:"absolute",bottom:"0",height:"30px",width:"100%"}}>
-			<div style={{marginLeft:"40%",fontSize:"19px"}}>Made with ❤️ by SudoCoders</div>
-  				
-      </div> */}
+	  <footer id="footer">
+			      <div className="footer-text">Made with ❤️ by SudoCoders</div>
 	  </footer>
   );
 };
