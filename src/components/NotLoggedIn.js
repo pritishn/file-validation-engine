8c94649e72ec3601fefb99ff095a8ec9ab186796
Mrutyunjay Dash <mdash555@gmail.com>
2020-11-22 21:49:23 +0530

    notloggedin page refactored

--- src/components/NotLoggedIn.js
+++ src/components/NotLoggedIn.js
@@ -3,14 +3,13 @@ import { Link } from 'react-router-dom';
 
 class NotLoggedIn extends Component {
 	render () {
-		//make navbar here
 		return (
 			<div className='NotLoggedIn card center'>
-				<p style={{ fontSize: '3em', fontWeight: '500', paddingTop: '5%' }}>
+				<p style={{ fontSize: '1.8em', fontWeight: '470', paddingTop: '5%' }}>
 					NOT LOGGED IN! <br /> Cannot Access This Feature!
 				</p>
 				<Link to='/'>
-					<a className='btn btn-large btn-waves black' style={{ borderRadius: '5px', fontSize: '1.1em' }}>
+					<a className='btn btn-large btn-waves dell-blue-dark' style={{ borderRadius: '5px', fontSize: '1.1em',margin:'20px' }}>
 						<b>Login</b>
 					</a>
 				</Link>
